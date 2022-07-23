efeac098767c6c6856a442b64858b65f0b8f114d
卫慧杰 <w920098695@sina.cn>
2022-07-23 22:56:39 +0800

    feat: 加载页面布局

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:moviesapp/screens/onboarding_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,11 +11,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-
-      home: Scaffold(),
+      theme: ThemeData(
+        fontFamily:  GoogleFonts.openSans().fontFamily,
+      ),
+      home: const OnboardingScreen(),
     );
   }
 }
