4ee5cd667e9f1a960f87d0dd22600e2088c26db2
卫慧杰 <w920098695@sina.cn>
2022-07-25 00:13:23 +0800

    feat: 电影详情页面

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:moviesapp/screens/home_screen.dart';
+import 'package:moviesapp/screens/movie_detail_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         fontFamily:  GoogleFonts.openSans().fontFamily,
       ),
-      home: const HomeScreen(),
+      home: const MovieDetailScreen(),
     );
   }
 }
